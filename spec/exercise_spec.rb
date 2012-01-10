cf64e93a0c123d6589c7089b8e515638f0fcfad0
Cox <jcox201@DIVWL-1318AGXA.cable.comcast.com>
2012-01-10 12:47:22 -0500

    Made the passing test fail

--- spec/exercise_spec.rb
+++ spec/exercise_spec.rb
@@ -1,7 +1,7 @@
 describe "String" do
   context "chop deletes the last character of a string" do
     it "deletes the last character" do
-      str = "chop!"
+      str = "chop!!"
 
       str.chop.should == "chop"
     end
